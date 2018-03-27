40b3bcbd77f8efe1899c88c045309851b82bf2a0
qwe7002 <qwe7002@hotmail.com>
2018-03-28 00:43:59 +0800

    实现post_list_view绑定

--- silverblog/ViewController.swift
+++ silverblog/ViewController.swift
@@ -15,10 +15,6 @@ class ViewController: UIViewController {
 
     @IBAction func on_enter_click(_ sender: Any) {
         self.view.endEditing(true)
-        global_value.server_url = server_name.text!
-        if (global_value.password == "" && password.text != ""){
-            global_value.password = public_func.md5(password.text!)
-        }
         if (global_value.server_url == "" || global_value.password == "") {
             let alertController = UIAlertController(title: "Error", message: "site address or password cannot be blank.", preferredStyle: UIAlertControllerStyle.alert)
             let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.default) { (ACTION) in
@@ -27,17 +23,24 @@ class ViewController: UIViewController {
             alertController.addAction(okAction);
             self.present(alertController, animated: true, completion: nil)
         }
-        shared.set(global_value.server_url, forKey: "server")
-        shared.set(global_value.password, forKey: "password")
-        shared.synchronize()
-        let alertController = UIAlertController(title: "Success", message: "Your settings have been saved.", preferredStyle: UIAlertControllerStyle.alert)
-        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.default) { (ACTION) in
-            let sb = UIStoryboard(name:"Main", bundle: nil)
-            let vc = sb.instantiateViewController(withIdentifier: "post_list") as! UITabBarController
-            self.present(vc, animated: true, completion: nil)
+        if (global_value.password != password.text!){
+            global_value.server_url = server_name.text!
+            global_value.password = public_func.md5(password.text!)
+            shared.set(global_value.server_url, forKey: "server")
+            shared.set(global_value.password, forKey: "password")
+            shared.synchronize()
+            let alertController = UIAlertController(title: "Success", message: "Your settings have been saved.", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.default) { (ACTION) in
+                let sb = UIStoryboard(name:"Main", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "post_list") as! UITabBarController
+                self.present(vc, animated: true, completion: nil)
+            }
+            alertController.addAction(okAction);
+            self.present(alertController, animated: true, completion: nil)
         }
-        alertController.addAction(okAction);
-        self.present(alertController, animated: true, completion: nil)
+        let sb = UIStoryboard(name:"Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "post_list") as! UITabBarController
+        self.present(vc, animated: true, completion: nil)
 
     }
 
@@ -47,6 +50,7 @@ class ViewController: UIViewController {
             global_value.server_url = shared.string(forKey: "server")!
             global_value.password = shared.string(forKey: "password")!
             server_name.text = global_value.server_url
+            password.text = global_value.password
         }
     }
 }
---
+++ silverblog/post_list_view.swift
@@ -0,0 +1,24 @@
+//
+//  post_list_view.swift
+//  silverblog
+//
+//  Created by 黄江华 on 2018/3/28.
+//  Copyright © 2018年 qwe7002. All rights reserved.
+//
+
+import UIKit
+
+class post_list_view: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("load")
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
