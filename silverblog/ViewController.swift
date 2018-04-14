5973a67955668db5599c881cca68e78aae0cad3f
qwe7002 <qwe7002@qwe7002-remote.local>
2018-04-14 15:55:07 +0800

    修复重复登录的bug

--- silverblog/ViewController.swift
+++ silverblog/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var server_name: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBAction func on_enter_click(_ sender: Any) {
-        global_value.password=public_func.md5(password.text!)
-        global_value.server_url=server_name.text!
+        if(password.text != global_value.password){
+            global_value.password=public_func.md5(password.text!)
+            global_value.server_url=server_name.text!
+        }
         save_info()
         self.view.endEditing(true)
         if (global_value.server_url == "" || global_value.password == "") {
