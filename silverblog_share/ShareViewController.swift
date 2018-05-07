67b3380ffbc708dd7aaf4b7406c9c44e43344ece
qwe7002 <qwe7002@hotmail.com>
2018-05-07 12:49:44 +0800

    修改提示框

--- silverblog_share/ShareViewController.swift
+++ silverblog_share/ShareViewController.swift
@@ -37,8 +37,14 @@ class ShareViewController: SLComposeServiceViewController {
         let password = shared.string(forKey: "password")!
         let server = shared.string(forKey: "server")!
         let sign = md5(post_title + password)
-        let alertController = UIAlertController(title: "Now publishing, please wait...", message: "", preferredStyle: .alert)
+        let alertController = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        loadingIndicator.startAnimating();
+        alertController.view.addSubview(loadingIndicator)
         self.present(alertController, animated: true, completion: nil)
+        
 
         let parameters: Parameters = [
             "title": post_title,
